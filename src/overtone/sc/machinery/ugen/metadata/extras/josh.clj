d4782c454d2a2da3542714ad0dd946c81a401a48
Steve Salevan <steve@tumblr.com>
2012-12-01 15:31:40 -0500

    Adding in lorquas' Josh UGens additions

---
+++ src/overtone/sc/machinery/ugen/metadata/extras/josh.clj
@@ -0,0 +1,17 @@
+(ns overtone.sc.machinery.ugen.metadata.extras.josh
+  (:use [overtone.sc.machinery.ugen common check]))
+
+(def specs
+  [
+   {:name "MoogVCF"
+    :summary
+    :args [{:name "input"
+            :doc "audio input"}
+
+           {:name "ffreq"
+            :doc "cutoff freq"}
+
+           {:name "res"
+            :doc "resonance (0-1)"}]
+    :rates #{:ar}
+    :doc  "MoogVCF is a port of the CSound opcode moogvcf written by Josep Comajuncosas and Hans Mikelson.  This UGen is a digital emulation of the Moog diode ladder filter configuration.  It is based loosely on the paper 'Analyzing the Moog VCF with Considerations for Digital Implemnetation'  by Stilson and Smith (CCRMA)."} ])
